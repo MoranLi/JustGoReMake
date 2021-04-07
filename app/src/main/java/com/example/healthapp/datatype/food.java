417bdbc9467cc49815f40b3a60d9bc71efb39296
Yukun Li <moran.li@usask.ca>
2021-04-07 16:33:50 -0600

    add food / exercise

---
+++ app/src/main/java/com/example/healthapp/datatype/food.java
@@ -0,0 +1,76 @@
+package com.example.healthapp.datatype;
+
+public class food {
+    private int id;
+    private int user_id;
+    private int category;
+    private String name;
+    private double protein;
+    private double fat;
+    private double cholesterol;
+    private double calories;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public void setCholesterol(double cholesterol) {
+        this.cholesterol = cholesterol;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public double getCholesterol() {
+        return cholesterol;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+}
