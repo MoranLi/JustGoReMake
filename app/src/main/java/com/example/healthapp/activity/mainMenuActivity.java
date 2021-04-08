c20d5459602827d6634298d5190a436f0aca0a4e
Yukun Li <moran.li@usask.ca>
2021-04-08 14:00:22 -0600

    main and relative page

--- app/src/main/java/com/example/healthapp/activity/mainMenuActivity.java
+++ app/src/main/java/com/example/healthapp/activity/mainMenuActivity.java
@@ -6,7 +6,10 @@ package com.example.healthapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.healthapp.R;
 
@@ -20,6 +23,61 @@ public class mainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ((Button)findViewById(R.id.go_to_food)).setOnClickListener(addFood());
+        ((Button)findViewById(R.id.go_to_exercise)).setOnClickListener(addExercise());
+        ((Button)findViewById(R.id.go_to_weight)).setOnClickListener(addWeight());
+        ((Button)findViewById(R.id.go_to_recommdation)).setOnClickListener(goRecommdation());
+        ((Button)findViewById(R.id.go_to_user_info)).setOnClickListener(changeProfile());
+    }
+
+    private View.OnClickListener addFood() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent unit_intent = new Intent(getItSelf(), foodActivity.class);
+                startActivity(unit_intent);
+            }
+        };
+    }
+
+    private View.OnClickListener addExercise() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent unit_intent = new Intent(getItSelf(), exerciseActivity.class);
+                startActivity(unit_intent);
+            }
+        };
+    }
+
+    private View.OnClickListener addWeight() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent unit_intent = new Intent(getItSelf(), weightActivity.class);
+                startActivity(unit_intent);
+            }
+        };
+    }
+
+    private View.OnClickListener goRecommdation() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent unit_intent = new Intent(getItSelf(), recommdationActivity.class);
+                startActivity(unit_intent);
+            }
+        };
+    }
+
+    private View.OnClickListener changeProfile() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent unit_intent = new Intent(getItSelf(), userInfoActivity.class);
+                startActivity(unit_intent);
+            }
+        };
     }
 
     /**
---
+++ app/src/main/java/com/example/healthapp/activity/recommdationActivity.java
@@ -0,0 +1,16 @@
+package com.example.healthapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthapp.R;
+
+public class recommdationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommdation);
+    }
+}
